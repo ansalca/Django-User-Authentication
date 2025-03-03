b1ca8732768597e1400d7bb11d84a5cb00f695a8
ansalca <ansalanu919@gmail.com>
2025-03-03 14:43:39 +0530

    default Django UserForm Extended Successfully

--- authentication/accounts/urls.py
+++ authentication/accounts/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register, user_login, user_logout, CustomPasswordResetView, CustomPasswordResetConfirmView
+from .views import register, user_login, user_logout, CustomPasswordResetView, CustomPasswordResetConfirmView, home, view_users, add_user, edit_user
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('', home, name='home'),
+    path('users/', view_users, name='view_users'),
+    path('users/add/', add_user, name='add_user'),
+    path('users/edit/<int:user_id>/', edit_user, name='edit_user'),
     path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
--- authentication/accounts/views.py
+++ authentication/accounts/views.py
@@ -1,14 +1,49 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
 from django.contrib import messages
 from .forms import RegisterForm, LoginForm, CustomPasswordResetForm, CustomSetPasswordForm
+from django.contrib.auth.decorators import login_required, permission_required
+
+
+from .models import Profile
 
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
+            department = form.cleaned_data.get('department')
+            date_of_birth = form.cleaned_data.get('date_of_birth')
+            address = form.cleaned_data.get('address')
+            Profile.objects.create(user=user, department=department, date_of_birth=date_of_birth, address=address)
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Unsuccessful registration. Invalid information.')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            group = form.cleaned_data.get('group')
+            user.groups.add(group)
             login(request, user)
             messages.success(request, 'Registration successful.')
             return redirect('home')
@@ -40,3 +75,38 @@ class CustomPasswordResetView(PasswordResetView):
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     form_class = CustomSetPasswordForm
     template_name = 'accounts/password_reset_confirm.html'
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+@permission_required('auth.view_user', raise_exception=True)
+def view_users(request):
+    users = User.objects.all()
+    return render(request, 'accounts/view_users.html', {'users': users})
+
+@login_required
+@permission_required('auth.change_user', raise_exception=True)
+def edit_user(request, user_id):
+    user = User.objects.get(id=user_id)
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('view_users')
+    else:
+        form = RegisterForm(instance=user)
+    return render(request, 'accounts/edit_user.html', {'form': form})
+
+@login_required
+@permission_required('auth.add_user', raise_exception=True)
+def add_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('view_users')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/add_user.html', {'form': form})
